780c42b1c4fd83aa69c1413782f1f65d9e18e6bd
Veeravel-S <veeravel.igt@gmail.com>
2025-10-15 11:56:58 +0530

    gtm issue fixed

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,12 +4,10 @@ import Header from "@/common/Header";
 import { Popup } from "@/common/Popup";
 import useUTMSource from "@/hooks/useUTMSource";
 import "@/styles/globals.css";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 
 export default function App({ Component, pageProps }) {
   useUTMSource();
-  const router = useRouter();
   const [open, setOpen] = useState(false);
   const handleTogglecontactForm = (value) => {
     if (typeof value === "boolean") {
@@ -19,30 +17,9 @@ export default function App({ Component, pageProps }) {
     }
   };
 
-  useEffect(() => {
-    const pushPageview = (url) => {
-      window.dataLayer = window.dataLayer || [];
-      window.dataLayer.push({
-        event: "pageview",
-        page: url,
-      });
-      console.log("GTM pageview event pushed:", url);
-    };
 
-    pushPageview(router.asPath);
-
-    const handleRouteChange = (url) => {
-      pushPageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events, router.asPath]);
   return (
     <>
-
       <Header handleTogglecontactForm={handleTogglecontactForm} />
 
       <Component {...pageProps} handleTogglecontactForm={handleTogglecontactForm} />
